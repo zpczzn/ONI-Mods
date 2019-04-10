3ef6c28ea77ec9d0c8ed4e98710a0b1ced70aa73
zpczzn <49393524+zpczzn@users.noreply.github.com>
2019-04-10 21:15:47 +0800

    BiggerStorage
    BiggerStorage

---
+++ BiggerStorageMod/BiggerStorageMod.cs
@@ -0,0 +1,16 @@
+﻿using Harmony;
+
+namespace BiggerStorageMod
+{
+    [HarmonyPatch(typeof(Immigration), "Sim200ms")]
+    internal class BiggerStorageMod_Immigration_Sim200ms
+    {
+        private static void Postfix(float dt,Immigration __instance,ref bool ___stopped,ref bool ___bImmigrantAvailable)
+        {
+            if (!___stopped && !___bImmigrantAvailable)
+            {
+                __instance.timeBeforeSpawn -= dt * 59f;
+            }
+        }
+    }
+}
